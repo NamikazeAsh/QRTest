a2f2977a864a2694ae8816c69fc1060f749e8b3d
NamikazeAsh <ashwinsatish0@gmail.com>
2023-06-04 20:25:40 +0530

    static files

--- DoctorApp/urls.py
+++ DoctorApp/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf import settings
+
 from . import views
 
 from django.conf.urls.static import static
@@ -25,5 +27,5 @@ urlpatterns = [
     
     path('',views.PatientSubmission),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
