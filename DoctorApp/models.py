50aa6534824d928c120b7185401dc85c5b78046e
NamikazeAsh <ashwinsatish0@gmail.com>
2023-07-19 21:53:50 +0530

    doctor model

--- DoctorApp/models.py
+++ DoctorApp/models.py
@@ -8,3 +8,14 @@ class PatientModel(models.Model):
     description = models.CharField(max_length=300)
     pdfs = models.FileField(null=True,upload_to="pdfs",validators=[FileExtensionValidator(['pdf',""])])
     qr = models.CharField(max_length=100,default="No",null=True,blank=True)
+    
+    
+class DoctorModel(models.Model):
+
+    name = models.CharField(max_length=100)
+    clinic = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=500)
+    experience = models.IntegerField()
+    address = models.CharField(max_length=100)
+    avail_timing = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
